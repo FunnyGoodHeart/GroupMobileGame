e88895211254cd0f37f68e2a699d9447663ade4a
polystation1207 <25.omar.fierro@ksd.org>
2024-02-01 09:08:10 -0800

    Updated level exit script to include a load delay

--- GroupMobileProject/Assets/Scripts/Triggers/levelExit.cs
+++ GroupMobileProject/Assets/Scripts/Triggers/levelExit.cs
@@ -8,13 +8,27 @@ public class levelExit : MonoBehaviour
     [SerializeField] bool isLastLevel = false;
     [Tooltip("Get scene number through build settings")]
     [SerializeField] int lastLevel = 1;
+    [SerializeField] float loadDelay = 1f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (isLastLevel == true)
         {
-            SceneManager.LoadScene(lastLevel);
+            Invoke("LastLevel", loadDelay);
         }
+        else
+        {
+            Invoke("NextLevel", loadDelay);
+        }
+    }
+
+    void LastLevel()
+    {
+        SceneManager.LoadScene(lastLevel);
+    }
+
+    void NextLevel()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
